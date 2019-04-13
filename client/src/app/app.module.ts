675c1515f88228323c77e619cfc345ae1ab50b27
masayuki038 <masayuki038@gmail.com>
2019-04-13 01:57:27 +0000

    Add components/services to authenticate user

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ApiService } from './api.service';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule }    from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '../../node_modules/@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '../../node_modules/@angular/common/http';
 import { HttpClientXsrfModule } from '../../node_modules/@angular/common/http';
 import { ContentListComponent } from './content-list/content-list.component';
 import { AppComponent } from './app.component';
@@ -11,15 +12,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ContentDetailComponent } from './content-detail/content-detail.component';
+import { AlertComponent } from './_components/alert.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './_guards/auth.guard';
+import { JwtInterceptor } from './_helpers/jwt.interceptor';
+import { ErrorInterceptor } from './_helpers/error.interceptor';
+import { fakeBackendProvider } from './_helpers/fake-backend';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 @NgModule({
   declarations: [ 
-    ContentListComponent,
     AppComponent,
+    AlertComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ContentListComponent,
     MessagesComponent,
     DashboardComponent,
-    ContentDetailComponent,
+    ContentDetailComponent
   ],
   imports: [
     AppRoutingModule,
@@ -32,9 +45,15 @@ import { MatButtonModule, MatCheckboxModule } from '@angular/material';
     HttpClientXsrfModule.withOptions({
       cookieName: 'X_CSRF_Token',
       headerName: 'X-CSRF-Token'
-    })
+    }),
+    ReactiveFormsModule
   ],
-  providers: [ApiService],
+  providers: [
+    ApiService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 
